a39154b10fa779245f800b600d7cf5db3f7bd943
Juanjhenao <juanjosehenao20@gmail.com>
2022-04-06 15:56:01 -0500

    overall finished design of the site, functionality in perfect condition

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ let computerSelection;
 let computerScore = 0
 let userScore = 0
 
+
+
+
 function button (tool){
     if (tool == 0){
         userSelection = scissrs
@@ -30,12 +33,41 @@ function button (tool){
     }
 }
 
+function restart (){
+    const userWin = document.querySelector('.user-score');      //--> prevents the page from displaying
+        userWin.textContent = `Your score: 0`;                  //--> an extra sum to the score
+    const pcWin = document.querySelector('.computer-score');
+        pcWin.textContent = `Computer score: 0`;
+        window.location.reload()
+
+}
+function closeModal(){
+    const closeModal = document.getElementById('modal-container');
+    closeModal.classList.remove('show');
+}
+
+function numberOf(points){
+    closeModal()
+    points == rounds
+    if (userScore == rounds){
+        // const final = document.querySelector('.final-winner');
+        // final.innerHTML = `You are the definitive winner <br> Select any tool to restart`;
+        alert("hols")
+        
+    }
+
+    else if (computerScore == rounds){
+        // const final = document.querySelector('.final-winner');
+        // final.innerHTML = `you really suck at this you should do something easier, like playing ukele for example <br> Select any tool to restart`;
+        alert("p")
+    }
+}
 
 function result(winner){
     if (winner == "user"){
         userScore++
         const userWin = document.querySelector('.user-score');
-        userWin.textContent = `Your score: ${userScore}`;
+        userWin.textContent = `Player: ${userScore} `;
 
         const who = document.querySelector('.winner');
         who.textContent = `You won!`;
@@ -44,7 +76,7 @@ function result(winner){
     else if (winner == "computer"){
         computerScore++
         const pcWin = document.querySelector('.computer-score');
-        pcWin.textContent = `Computer score: ${computerScore}`;
+        pcWin.textContent = `Computer: ${computerScore} `;
 
         const who = document.querySelector('.winner');
         who.textContent = `You lose :(`;
@@ -57,49 +89,103 @@ function result(winner){
 }
 
 
+// function numberOf(points){                   no funciona xd
+//     if (userScore == points){
+//         alert("sos dios :v")
+//     }
+
+//     else if (computerScore == points){
+//         alert("eres una mierda :V")
+//     }
+// }
+
+// numberOf(5)
 
 
 
+    
+function userImage(image){
+    if (image == "paper"){
+        let userChoose = document.getElementById("user-choose");
+        userChoose.src = 'images/paper.png';
+    }
+    else if (image == "rock"){
+        let userChoose = document.getElementById("user-choose");
+        userChoose.src = 'images/rock.png';
+    }
+    else if (image == "scissrs"){
+        let userChoose = document.getElementById("user-choose");
+        userChoose.src = 'images/scissors.png';
+    }
+}
+
+function computerImage(image){
+    if (image == "paper"){
+        let userChoose = document.getElementById("computer-choose");
+        userChoose.src = 'images/paper.png';
+    }
+    else if (image == "rock"){
+        let userChoose = document.getElementById("computer-choose");
+        userChoose.src = 'images/rock.png';
+    }
+    else if (image == "scissrs"){
+        let userChoose = document.getElementById("computer-choose");
+        userChoose.src = 'images/scissors.png';
+    }
+}
 function round (computerSelection){                        //--> Play round
     computerSelection = nrandom(0,2)
 
-    const userTool = document.querySelector('.user-choose');
-    userTool.textContent = `Player: ${options[userSelection]}`;
+    // const userTool = document.querySelector('.user-choose');
+    // userTool.innerHTML = `Player: ${options[userSelection]}`;
 
-    const computerTool = document.querySelector('.computer-choose')
-    computerTool.textContent = `Computer: ${options[computerSelection]}`
+    // const computerTool = document.querySelector('.computer-choose')
+    // computerTool.textContent = `Computer: ${options[computerSelection]}`
 
+  
 if (userSelection == scissrs){
+    userImage("scissrs");
     if (computerSelection == scissrs){
-        result("tie")
+        computerImage("scissrs");
+        result("tie");
         }
     else if (computerSelection == paper){
-        result("user")
+        computerImage("paper");
+        result("user");
     }
     else if (computerSelection == rock){
-        result("computer")
+        computerImage("rock");
+        result("computer");
     }
 }
 else if (userSelection == paper){
+    userImage("paper")
     if (computerSelection == paper){
-        result("tie")
+        computerImage("paper");
+        result("tie");
         }
     else if (computerSelection == rock){
-        result("user")
+        computerImage("rock");
+        result("user");
     }
     else if (computerSelection == scissrs){
-        result("computer")
+        computerImage("scissrs");
+        result("computer");
     }
 }
 else if (userSelection == rock){
+    userImage("rock")
     if (computerSelection == rock){
-        result("tie")
+        computerImage("rock");
+        result("tie");
     }
     else if (computerSelection == scissrs){
-        result("user")
+        computerImage("scissrs");
+        result("user");
     }
     else if (computerSelection == paper){
-        result("computer")
+        computerImage("paper");
+        result("computer");
     }
 }
 }
